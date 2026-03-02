5739c43aad267918f8f0b9428404899c8f4af486
PlayingJM098 <rahonjm@gmail.com>
2026-03-02 18:49:04 +0800

    Add files via upload

---
+++ src/main/java/multimine/app/multimineapp/MainController.java
@@ -0,0 +1,26 @@
+package multimine.app.multimineapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+
+import java.io.IOException;
+
+public class MainController {
+
+    public void openHelp(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(
+                getClass().getResource("help.fxml")
+        );
+
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+        stage.setTitle("How to Play");
+        stage.show();
+    }
+}
