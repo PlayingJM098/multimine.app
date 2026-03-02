6e3d80736be4c9b621bbce83054e918b7b03c116
PlayingJM098 <rahonjm@gmail.com>
2026-03-02 18:52:47 +0800

    Delete multimine.app-main directory

--- multimine.app-main/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module multimine.app.multimineapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens multimine.app.multimineapp to javafx.fxml;
-    exports multimine.app.multimineapp;
-}
