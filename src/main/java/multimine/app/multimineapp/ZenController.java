90c52b275b1730d2d50f357b85a3592cdb19a9c0
nocalbe <caleblucas.esteban@gmail.com>
2026-03-04 20:57:08 +0800

    Update ZenController.java

--- src/main/java/multimine/app/multimineapp/ZenController.java
+++ src/main/java/multimine/app/multimineapp/ZenController.java
@@ -8,6 +8,31 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ZenController {
-    
+    @FXML
+    private GridPane grid; // from fxml
+
+    private ImageView[][] board; // 2d arrray initializationfgfdd
+
+    @FXML
+    public void initialize() {
+
+    int rows = 15;
+    int cols = 15;
+
+        board = new ImageView[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                ImageView imageView = new ImageView();
+                imageView.setFitWidth(50);
+                imageView.setFitHeight(50);
+
+                board[row][col] = imageView;
+
+                grid.add(imageView, col, row); 
+            }
+        }
+}
     
 }
