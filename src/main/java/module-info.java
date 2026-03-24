a35907961ab7fbf5c91eccfe8214768b12533d79
PlayingJM098 <rahonjm@gmail.com>
2026-03-24 13:44:27 +0800

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module multimine.app.multimineapp {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens multimine.app.multimineapp to javafx.fxml;
+
     exports multimine.app.multimineapp;
+    exports model;
 }
