5599aca5a14dc573b48eea91f5e4da013864b575
PlayingJM098 <rahonjm@gmail.com>
2026-04-21 19:52:43 +0800

    disabled maximized and resizing

--- src/main/java/multimine/app/multimineapp/Main.java
+++ src/main/java/multimine/app/multimineapp/Main.java
@@ -1,22 +1,20 @@
 package multimine.app.multimineapp;
 
+//Truth - Domingo, Esteban, Rahon
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-/* Screens submitted
-    Main Scene (Main.fxml)
-    Zen Game Summary Scene (zensum.fxml)
-    Help/Instructions Scene (help.fxml)*/
+
 public class Main extends Application {
     @Override
-   
     public void start(Stage primaryStage) throws Exception {
-                                                  //change the filename to load other screens                               
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
-        Scene scene = new Scene(loader.load(), 470, 400);
+        Scene scene = new Scene(loader.load(), 474, 330);
         primaryStage.setTitle("Main Menu");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);   
+        primaryStage.setMaximized(false);
         primaryStage.show();
     }
 
